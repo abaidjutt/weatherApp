002b0ca1fd830f3ec8a257995cb4003210291b9e
abaidjutt <abaidjutt784@gmail.com>
2025-07-08 23:47:25 +0500

    weatherApp
    make wearther app take city name from user

---
+++ index.js
@@ -0,0 +1,7 @@
+function weatherApp() {
+    const cityEnter = document.getElementById("city");
+    const city = cityEnter.value;
+
+    const weatref = document.getElementById("weaterResult");
+    weatref.innerHTML = "submitted succesful";
+}
