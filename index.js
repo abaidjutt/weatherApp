429ba98dc8cc95e119f7cbf63433d9ff3146ec24
abaidjutt <abaidjutt784@gmail.com>
2025-07-09 21:57:49 +0500

    weather-app
    make a weather app that tell the weather  of every city which is enter by the user

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function weatherApp() {
+async function weatherApp() {
     const cityEnter = document.getElementById("city");
+    const outputRef = document.getElementById("weaterResult");
     const city = cityEnter.value;
 
-    const weatref = document.getElementById("weaterResult");
-    weatref.innerHTML = "submitted succesful";
+    const weatherRes = await fetch(`https://p2pclouds.up.railway.app/v1/learn/weather?city=${city}`);
+    const weather = await weatherRes.json();
+
+    const currentTemp = weather.current.temp_c;
+    outputRef.innerHTML = `Today temperature of ${city} is ${currentTemp}`;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
